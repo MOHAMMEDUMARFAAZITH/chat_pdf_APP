cc4b98f6411e28481c9c32fe571b779b09047282
MOHAMMED UMAR FAAZITH K <123172380+MOHAMMEDUMARFAAZITH@users.noreply.github.com>
2023-11-03 22:22:18 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -43,20 +43,23 @@ def main():
 	pdf = st.file_uploader("UPLOAD YOUR PDF",type='pdf')
 	st.balloons()
 	
-#pdf is none it will create some error
+	#pdf is none it will create some error
 	if pdf is not None:
 		#PdfReader - Used for to read the content in the PDF
 		pdf_reader = PdfReader(pdf)
 		text = ""
 		for page in pdf_reader.pages:
 			text += page.extract_text()
+			
+		#Split into Chunks
+                #Each chunk will contain 1000 tokens, with 200 tokens overlapped to keep the chunks related and prevent separation
 		text_splitter = RecursiveCharacterTextSplitter(
 			chunk_size = 1000,
 			chunk_overlap = 200,
 			length_function=len)
-
 		chunks = text_splitter.split_text(text=text)
-
+		
+		#Word Embedding - huggingface APIs
 		model_id = "sentence-transformers/all-MiniLM-L6-v2"
 		hf_token = "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"
 
@@ -68,47 +71,40 @@ def main():
   			return response.json()
 		with st.spinner('Wait for it...'):
     				time.sleep(5)
-		st.success('This is a success message!', icon="✅")	
-
+		st.success('This is a success message!', icon="✅")
+		
+		#Enter your question here
 		user_question= st.text_input("Enter your question here ?")
 		st.write(user_question)
 		st.snow()
 		
 		
-
 		try:
-
 			question = query([user_question])
-            
-			query_embeddings = torch.FloatTensor(question)
-    
-			output=query(chunks)
-    
-			output=torch.from_numpy(np.array(output)).to(torch.float)
-
+            		query_embeddings = torch.FloatTensor(question)
+    			output=query(chunks)
+    			output=torch.from_numpy(np.array(output)).to(torch.float)
+			
+			#Semantic Search
 			result=util.semantic_search(query_embeddings, output,top_k=2)
-
 			final=[chunks[result[0][i]['corpus_id']] for i in range(len(result[0]))]
-	
-			AI21_api_key = 'q6QM3NWgec9PUO85V0shxwo0QueC9QXe'
-
-			url = "https://api.ai21.com/studio/v1/answer"
 		except:
 			st.write("Upload your valid PDF file")
-    
+			
+    			#AI21studio Question Answer model
+			AI21_api_key = 'q6QM3NWgec9PUO85V0shxwo0QueC9QXe'
+			url = "https://api.ai21.com/studio/v1/answer"
 		try:
 			payload = {
                 		"context":' '.join(final),
                 		"question":user_question
           			}
-  
-			headers = {
+  			headers = {
                 		"accept": "application/json",
                 		"content-type": "application/json",
                 		"Authorization": f"Bearer {AI21_api_key}"
           			}
-    
-			response = requests.post(url, json=payload, headers=headers)
+    			response = requests.post(url, json=payload, headers=headers)
 		except:
 			st.write("Upload your valid PDF file")
     
@@ -122,8 +118,6 @@ def main():
      				st.write('The answer is not found  in the document ⚠️,please reformulate your question.')
 		except:
 			st.write("Please enter your question first.......")
-		
-
 
 if __name__ == '__main__': 
 	main()
